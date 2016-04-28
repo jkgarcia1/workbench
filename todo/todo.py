b5b40570a0070b97705f39d54a39b8111e193e80
Mark Seaman <markseaman@iMac>
2016-04-28 09:45:34 -0600

    Create todo list app logic and path setting example

--- todo/todo.py
+++ todo/todo.py
@@ -1,5 +1,19 @@
+#!/usr/bin/env python
+
 from sys import argv
+from os import system
 
-print('To Do List: '+' '.join(argv[1:]))
 
-open('ToDo.txt', 'w+').write(' '.join(argv[1:]))
+# Extract the input arguments to form new todo item
+text = ' '.join(argv[1:])[:80]
+print('To Do List: ' + text)
+
+# Append to file
+f = 'ToDo.txt'
+open(f, 'a').write(text+'\n')
+
+# Show the items
+print(open(f).read())
+
+# Edit the items
+system('e '+f)
